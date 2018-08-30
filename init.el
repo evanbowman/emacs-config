e2f145fb91bc006b69397741cddd5746fa56da68
ebowman <ebowman@limebrokerage.com>
2018-08-30 09:55:05 -0400

    fix subsitution in init

--- init.el
+++ init.el
@@ -13,7 +13,7 @@
                    "keys"))
   (let ((path (concat "~/emacs-config/" element)))
     (when (not (load path))
-      (error "failed to load %") path)))
+      (error "failed to load %s") path)))
 
 ;; I still like google better
 (setq eww-search-prefix "https://www.google.com/search?q=")
